a4330ee97d94f40a86d0bf8e285485899499e163
Tarente Lorian <lorian.tarente@gmail.com>
2023-02-20 10:29:01 +0100

    adding template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+import "./App.scss";
+
+function App(): ReactElement {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
