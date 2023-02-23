8cd2802a6386d6c2405761aeb99930c5ab031606
Tarente Lorian <lorian.tarente@gmail.com>
2023-02-23 15:49:31 +0100

    carousel base setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import "./App.scss";
 import Carousel from "./components/carousel/Carousel";
 
 function App(): ReactElement {
+  const pictures = [
+    {id: 1, img:"https://plus.unsplash.com/premium_photo-1669083824032-ce43d1b2df55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", alt: "image"},
+    {id: 2, img:"https://images.unsplash.com/photo-1676920410933-b8e5571e6f89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", alt: "image"},
+    {id: 3, img:"https://plus.unsplash.com/premium_photo-1666544989783-13fc7091024f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60", alt: "image"},
+    {id: 4, img:"https://images.unsplash.com/photo-1676836957114-2156d8deaf3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: "image"},
+    {id: 5, img:"https://images.unsplash.com/photo-1676804957763-0512ecd30795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: "image"}
+  ];
+  const control = {
+    show: true,
+    disabled: false
+  };
+
   return (
     <div className="App">
-      <Carousel/>
+      <Carousel
+        content= {pictures}
+        auto= {true}
+        time= {2000}
+        color= "white"
+        control= {control}
+        dot= {true}
+        transition= "linear"
+      />
     </div>
   );
 }
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -10,11 +10,12 @@ import React, { ReactElement, useState } from "react";
 import "./Carousel.scss";
 
 interface ContentProps {
-    title: string,
-    description: string,
+    title?: string,
+    description?: string,
     img: string,
     alt: string,
-    url: string
+    url?: string,
+    id: number
 }
 
 interface ControlProps{
@@ -23,54 +24,55 @@ interface ControlProps{
 }
 
 interface CarouselProps {
-    content: ContentProps,
+    content: ContentProps[],
     auto: boolean,
     time: number,
     color: string,
     control: ControlProps,
     dot: boolean,
-    transition: string
-    onChange: (e: any) => void,
-    onClick: (e: any) => void
+    transition: string,
+    onChange?: (e: any) => void,
+    onClick?: (e: any) => void
 }
 
-const Pictures = [
-  {id: 1, url:"https://plus.unsplash.com/premium_photo-1669083824032-ce43d1b2df55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"},
-  {id: 2, url:"https://images.unsplash.com/photo-1676920410933-b8e5571e6f89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"},
-  {id: 3, url:"https://plus.unsplash.com/premium_photo-1666544989783-13fc7091024f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"},
-  {id: 4, url:"https://images.unsplash.com/photo-1676836957114-2156d8deaf3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNzN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"},
-  {id: 5, url:"https://images.unsplash.com/photo-1676804957763-0512ecd30795?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"}
-];
-
 function Carousel(props: CarouselProps): ReactElement {
-  const [card, setCard] = useState([]);
+  const [card, setCard] = useState(props.content);
   const [picture, setPicture] = useState(0);
   
-
   const changeNext = (e: any): void => {
     picture >= card.length - 1 ? setPicture(0) : setPicture(picture + 1);
-    props.onChange(e);
+    if (props.onChange) props.onChange(e);
   };
   const changePrev = (e: any): void => {
     picture <= 0 ? setPicture(card.length - 1) : setPicture(picture - 1);
-    props.onChange(e);
+    if (props.onChange) props.onChange(e);
   };
   
+  const currentContent = props.content[picture];
+  
+
   return (
     <div className="carouselKit">
+      <h2>Carousel</h2>
       <div className="picture-container">
-        {Pictures.map((image, index): ReactElement => {
-          return (
-            <img  key={image.id}
-                  src={image.url}
-                  alt={`Image ${image.id}`}
-                  className={index === picture ? "active" : ""} />
-          );
-        })}
+        {currentContent && (
+          <img  key={currentContent.id}
+                src={currentContent.img}
+                alt={`Image ${currentContent.id}`}/>
+        )}
       </div>
       <div className="btnCarousel">
-        <button className="btnNext" onClick={changeNext}>Next</button>
-        <button className="btnPrev" onClick={changePrev}>Prev</button>
+        <button className="btnPrev" onClick={changePrev}>&#8592;</button>
+        <button className="btnNext" onClick={changeNext}>&#8594;</button>
+      </div>
+      <div className="btnSelect">
+        {card.map((item, index) => (
+          <button 
+            key={item.id}
+            style={index === picture ? {opacity: 1, width: "12px", height: "12px"} : {}}
+            onClick={(): void => setPicture(index)}>
+          </button>
+        ))}
       </div>
     </div>
   );
